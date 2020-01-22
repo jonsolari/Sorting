97e5b2cc783102b7bbcca56a55ccd26271c22650
System Administrator <jon.s.solari@gmail.com>
2020-01-22 09:27:22 -0500

    :art: merge working
    merge working

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,12 +3,21 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    for i in arrA:
-            if arrA[i] < arrB[0]:
-                merged_arr.append(arrA[i])
-            else:
-                merged_arr.append(arrB[0])
-                
+    a = 0
+    b = 0
+    for i in range(0, elements):
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a] 
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
     return merged_arr
 
 
